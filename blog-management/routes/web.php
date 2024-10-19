c11534c78cef35db140158dc59959149690f48ba
mahdirnma <rahnamamahdi78@gmail.com>
2024-10-19 20:30:43 +0330

    create login and register panel

--- blog-management/routes/web.php
+++ blog-management/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index'])->name('home')->middleware('auth');
+Route::get('login', [UserController::class, 'login'])->name('login.show');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
