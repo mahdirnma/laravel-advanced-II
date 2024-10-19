f7d4808d2e8ffc22976aca4c6beb5374d3ddb956
mahdirnma <rahnamamahdi78@gmail.com>
2024-10-19 20:42:03 +0330

    build login and logout process

--- blog-management/app/Http/Controllers/UserController.php
+++ blog-management/app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.home');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
 }
