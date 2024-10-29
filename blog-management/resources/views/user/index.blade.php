0e5398b300b07d657633db6ddcd16ba82e2ced51
mahdirnma <rahnamamahdi78@gmail.com>
2024-10-29 19:59:13 +0330

    design header og main user panel

--- blog-management/resources/views/user/index.blade.php
+++ blog-management/resources/views/user/index.blade.php
@@ -5,10 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MunMahdi</title>
     @vite('resources/css/app.css')
 </head>
 <body>
+<header class="w-svw h-20 flex justify-center bg-amber-500">
+    <div class="w-10/12 h-full flex bg-amber-700">
+        <div class="w-1/5 h-full flex items-center bg-blue-800">
+            <h1 class="font-bold text-2xl">MunMahdi</h1>
+        </div>
+        <nav class="w-3/5 h-full bg-blue-500 flex items-center justify-center">
+            <ul>
+                <li><a href="{{route('user.index')}}" class="font-semibold">Home</a></li>
+            </ul>
+        </nav>
+        <div class="w-1/5 h-full bg-blue-800 flex items-center justify-end">
+            <ul class="flex">
+                <li class="ml-3"><a href=""><img src="images/icons8-insta.svg" alt="" class="w-6"></a></li>
+                <li class="ml-3"><a href=""><img src="images/icons8-x.svg" alt="" class="w-5"></a></li>
+                <li class="ml-3"><a href=""><img src="images/icons8-search.svg" alt="" class="w-5"></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+</body>
+</html>
+{{--<body>
 <table>
     <tr class="border-2 border-black">
         <td class="font-bold border-2 border-black">blog title</td>
@@ -62,24 +84,5 @@
         </tr>
     @endforeach
 </table>
-</body>
-</html>
-{{--
-@foreach($categories as $category)
-    {{$category}}
-@endforeach
-<br>
-@foreach($blogs as $blog)
-    {{$blog}}
-@endforeach
-<br>
-@foreach($tags as $tag)
-    {{$tag}}
-    <br>
-@endforeach
-<form action="{{route('user.blog',['blog'=>$blogs[0]])}}" method="get">
-    <button type="submit">go</button>
-</form>
-
---}}
+</body>--}}
 
