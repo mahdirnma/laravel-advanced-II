964b9e3bdfc6f589dd20cdded5f0716e5715b74b
mahdirnma <rahnamamahdi78@gmail.com>
2024-11-15 11:43:49 +0330

    create checkSubscription middleware

--- manage-gym/app/Http/Kernel.php
+++ manage-gym/app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkRole' => \App\Http\Middleware\CheckRole::class,
+        'checkSubscription' => \App\Http\Middleware\CheckSubscription::class,
     ];
 }
