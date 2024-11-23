72f4768845ac68cba07503ba06a6456c1cf123bc
mahdirnma <rahnamamahdi78@gmail.com>
2024-11-23 23:22:30 +0330

    create login process

--- car-services/app/Http/Controllers/AuthController.php
+++ car-services/app/Http/Controllers/AuthController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserLoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(StoreUserLoginRequest $request){
+        $myData = $request->only('email', 'password');
+        if(Auth::attempt($myData)){
+            return to_route('index');
+        }else{
+            return redirect()->back();
+        }
+    }
 }
--- car-services/routes/web.php
+++ car-services/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[UserController::class,'index'])->name('index')->middleware('auth');
+Route::get('/login',[UserController::class,'login'])->name('login.show');
+Route::post('/login',[AuthController::class,'login'])->name('login');
+Route::get('/register',[UserController::class,'register'])->name('register.show');
+Route::post('/register',[AuthController::class,'register'])->name('register');
