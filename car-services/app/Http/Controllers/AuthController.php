132b9de5a41eb4a5e5cb5d17d664aeaab22df880
mahdirnma <rahnamamahdi78@gmail.com>
2024-11-23 23:33:16 +0330

    create register process

--- car-services/app/Http/Controllers/AuthController.php
+++ car-services/app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserLoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,5 +17,23 @@ class AuthController extends Controller
         }else{
             return redirect()->back();
         }
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        if ($request->password==$request->confirmPassword){
+            $user=User::create($request->validated());
+            if($user){
+                Auth::login($user);
+                return to_route('index');
+            }else{
+                return redirect()->back();
+            }
+        }else{
+            return redirect()->back();
+        }
+    }
+    public function logout(){
+        Auth::logout();
     }
 }
--- car-services/app/Http/Controllers/UserController.php
+++ car-services/app/Http/Controllers/UserController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.index');
+    }
+    public function login()
+    {
+        return view('auth.login');
+    }
+    public function register(){
+        return view('auth.register');
+    }
 }
